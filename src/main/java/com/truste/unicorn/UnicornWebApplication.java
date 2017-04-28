ebab050847b8575df0c794d55a72808bd5e0e990
Giridhar Nandigam <gnandigam@truste.com>
2017-04-28 16:00:39 -0700

    Initial Commit

---
+++ src/main/java/com/truste/unicorn/UnicornWebApplication.java
@@ -0,0 +1,12 @@
+package com.truste.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicornWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/truste/unicorn/UnicornWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.truste.unicorn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnicornWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
